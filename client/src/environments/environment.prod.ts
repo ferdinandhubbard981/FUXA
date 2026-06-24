f8948722c2b0847f4686c08efd5be9e211a2cdaf
ferdinand <ferdinandhubbard@gmail.com>
2026-06-24 17:58:31 +0200

    removed halo

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ declare function require(moduleName: string): any;
 export const environment = {
   version: require('../../package.json').version,
   production: true,
-  apiEndpoint: null,
+  apiEndpoint: '10.30.12.255',
   apiPort: null,
   serverEnabled: true,
   type: null
